390720b89f006ce97a6297d99c969990805f87ab
baljitp <baljit.pal@chelsea-apps.com>
2017-01-09 17:39:12 +0000

    cleaned up url for proxies

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,21 @@ var router = express.Router();
 var fetch = require('node-fetch');
 var parser = require('xml2json');
 
+// const BASEURL = 'https://www.goodreads.com/search.xml?key=PffdULAudc8ss8djUWWpdw&q='
+
+const key = 'PffdULAudc8ss8djUWWpdw';
+// const BASEURL = `https://www.goodreads.com/search.xml?key=${key}&q=Ender%27s+Game`;
+const BASEURL = `https://www.goodreads.com/`;
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
-let url = 'https://www.goodreads.com/search.xml?key=PffdULAudc8ss8djUWWpdw&q=Ender%27s+Game';
-/* GET home page. */
+
+// dummy search url for now, needs to be improved
+let url = `${BASEURL}search.xml?key=${key}&q=Ender%27s+Game`;
+
 router.get('/book', function(req, res) {
   return getBook()
         .then(_returnSuccess);
@@ -22,9 +31,7 @@ router.get('/book', function(req, res) {
          return Promise.resolve(json);
      });
   }
-
   function _returnSuccess(data) {
-    console.log('data::', data);
     res.send(data);
   }
 });
